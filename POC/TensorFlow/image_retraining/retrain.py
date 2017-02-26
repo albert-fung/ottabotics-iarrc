8bac331b81457f3cd9c60b1a41ea24b139987bd9
jimmy <jimde@users.noreply.github.com>
2017-02-25 23:33:39 -0500

    modified retrain.py to download the model if it doesn't exist locally

--- POC/TensorFlow/image_retraining/retrain.py
+++ POC/TensorFlow/image_retraining/retrain.py
@@ -13,13 +13,47 @@ from sklearn.svm import SVC, LinearSVC
 import matplotlib.pyplot as plt
 #%matplotlib inline
 import pickle
+
 import time
+from subprocess import call
+import tarfile
+import os.path
+from six.moves import urllib
+import sys
+
 
 model_dir = 'imagenet/'
 images_dir = 'images/'
 list_images = [images_dir+f for f in os.listdir(images_dir) if re.search('jpg|JPG', f)]
 
+DATA_URL = 'http://download.tensorflow.org/models/image/imagenet/inception-2015-12-05.tgz'
+
 # print(">>> list of image:",list_images)
+
+
+def maybe_download_and_extract():
+    """Download and extract model tar file."""
+    dest_directory = "imagenet/"
+    print("FLAGS.model_dir: ", dest_directory)
+    if not os.path.exists(dest_directory):
+        os.makedirs(dest_directory)
+    filename = DATA_URL.split('/')[-1]
+    filepath = os.path.join(dest_directory, filename)
+    if not os.path.exists(filepath):
+        def _progress(count, block_size, total_size):
+          sys.stdout.write('\r>> Downloading %s %.1f%%' % (
+              filename, float(count * block_size) / float(total_size) * 100.0))
+          sys.stdout.flush()
+        filepath, _ = urllib.request.urlretrieve(DATA_URL, filepath, _progress)
+        print()
+        statinfo = os.stat(filepath)
+        print('Successfully downloaded', filename, statinfo.st_size, 'bytes.')
+    tarfile.open(filepath, 'r:gz').extractall(dest_directory)
+    
+maybe_download_and_extract()
+
+
+
 
 def create_graph():
     with gfile.FastGFile(os.path.join(
